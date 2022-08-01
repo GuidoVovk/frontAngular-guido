08ad16d41140b6a6712964a57c01dc22b5e09049
GuidoVovk <guido_z86@hotmail.com>
2022-08-01 16:15:13 -0300

    sobre-mi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    SobreMiComponent
   ],
   imports: [
     BrowserModule,
