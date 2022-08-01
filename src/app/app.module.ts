c35bfd81b4ad90ce89e8177930416ff7b60929c5
GuidoVovk <guido_z86@hotmail.com>
2022-08-01 16:41:52 -0300

    componente estudios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
 import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EstudiosComponent } from './componentes/estudios/estudios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EncabezadoComponent,
     SobreMiComponent,
-    ExperienciaComponent
+    ExperienciaComponent,
+    EstudiosComponent
   ],
   imports: [
     BrowserModule,
