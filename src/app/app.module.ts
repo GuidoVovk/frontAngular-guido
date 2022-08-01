716d643a864f8e7d98b0b5db1934d6a5fe618f4f
GuidoVovk <guido_z86@hotmail.com>
2022-08-01 16:31:57 -0300

    componente experiencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
 import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EncabezadoComponent,
-    SobreMiComponent
+    SobreMiComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
